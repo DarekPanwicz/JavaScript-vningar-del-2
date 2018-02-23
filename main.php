0762c3437fe7a042a6eb70739ffae5397fcce133
Darek P <dajmos007@gmail.com>
2018-02-23 13:08:29 +0100

    adding new code

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+1. Zapytaj o imie
+2. wyswietl komunikat "[WCIŚNIJ ENTER, ABY RZUCIĆ KOSTKĄ]"
+3. Gdy klient wcisnie enter niech wyswietli sie wartosc dla gracza i komputera
+Jeśli wartość gracza KOMPUTER jest większa, wygrywa KOMPUTER.
+Jeśli wartość gracza, którym jest użytkownik, jest większa, wygrywa użytkownik.
+W przypadku jednakowych wartości, jest remis
