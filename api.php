4d400a1e09f5b6ac235fe5ead96db225da927982
Darek P <dajmos007@gmail.com>
2018-02-24 13:19:34 +0100

    New file api.php

--- api.php
+++ api.php
@@ -5,3 +5,23 @@
  * Date: 2018-02-24
  * Time: 12:58
  */
+$method= $_SERVER["REQUEST_METHOD"];
+
+switch ($method) {
+        case 'GET':
+        echo 'GET';
+        break;
+
+        case 'POST':
+        echo 'POST';
+        break;
+
+        case 'PUT':
+        echo 'PUT';
+         break;
+
+        case 'DELETE':
+        echo 'DELETE';
+         break;
+
+}
